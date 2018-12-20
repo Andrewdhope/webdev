72b3a1d154ed461547dc018e27b72b3a1f7bad38
Andrew Hope <andrewdhope@gmail.com>
2018-12-20 14:27:30 -0800

    try dynamic menu

--- siteprep/scripts/vars.js
+++ siteprep/scripts/vars.js
@@ -3,11 +3,11 @@ var xmlpath, xslpath ;
 function setPath(setting) {
 	if (setting == "work") {
 		xmlpath = "xml/career.xml"
-		xslpath = "xml/transform.xml"
+		xslpath = "xml/transform.xsl"
 	}
 	
 	if (setting == "life") {
 		xmlpath = "xml/life.xml"
-		xslpath = "xml/transform.xml"
+		xslpath = "xml/transform.xsl"
 	}
 }
