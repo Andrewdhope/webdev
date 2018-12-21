1910194558fb2b227b4ce7ba840e4d0037a9c931
andrewdhope <andrewdhope@gmail.com>
2018-12-21 14:20:30 -0800

    valid xml

--- siteprep/scripts/vars.js
+++ siteprep/scripts/vars.js
@@ -3,11 +3,11 @@ var xmlpath, xslpath ;
 function setPath(setting) {
 	if (setting == "work") {
 		xmlpath = "xml/career.xml"
-		xslpath = "xml/transform.xsl"
+		xslpath = "xml/transform-work.xsl"
 	}
 	
 	if (setting == "life") {
 		xmlpath = "xml/leisure.xml"
-		xslpath = "xml/transform.xsl"
+		xslpath = "xml/transform-life.xsl"
 	}
 }
