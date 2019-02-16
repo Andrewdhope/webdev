3ca5d0a7457336bdaa43cac6915b7cc06bb29e2e
Andrew Hope <andrewdhope@gmail.com>
2019-02-15 21:31:36 -0800

    bounce up

--- siteprep/scripts/jQueryEffects.js
+++ siteprep/scripts/jQueryEffects.js
@@ -2,7 +2,7 @@ function jBounceUp() {
 		$("header").addClass("up")
 			.css("padding-top","20px")
 			.animate({bottom: '90%'}, "slow", "ajaxLoad(xmlpath,buildMenu,'xml/loadMenu.xsl',['#'])")
-			.css("transform": "none", "position": "static", "right": "0%");
+			.css({"transform": "none", "position": "static", "right": "0%"});
 		// if the class is flagged, just call ajaxLoad
 		// else set the attribute flag (addClass), and call animate with ajaxLoad as a callback
 		
