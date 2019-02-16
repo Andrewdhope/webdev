0bccc68121f44ba5d70e8bb647ba07c59219c26a
Andrew Hope <andrewdhope@gmail.com>
2019-02-16 14:36:21 -0800

    bounce work

--- siteprep/scripts/jQueryEffects.js
+++ siteprep/scripts/jQueryEffects.js
@@ -1,10 +1,19 @@
 function jBounceUp() {
+	if (document.getElementsByTagName("header")[0].classList.contains("up")) {
+		ajaxLoad(xmlpath,buildMenu,'xml/loadMenu.xsl',['#']);
+	} else {
 		$("header").addClass("up")
-			.animate({paddingTop: '54.2px'}, "slow") /* padding here is [2*(height of the header element)+2*(body's top margin)] */
-			.animate({bottom: '100%'}, "slow", function() {
-				$("header").removeClass("down").css({paddingTop: '10px'}) /* paddingTop effects needed to seamlessly switch the header element from absoute to static position. */
-				ajaxLoad(xmlpath,buildMenu,'xml/loadMenu.xsl',['#'])
-			});
 			
+			/* padding here set to [(height of the header element)+2*(body's top margin)+2*(desired padding after animation)] */
+			.animate({paddingTop: '55.2px'}, "slow") 
+			
+			.animate({bottom: '100%'}, "slow", function() {
+				
+				/* paddingTop effects are needed to seamlessly switch the header element from absoute to static position. */
+				$("header").removeClass("down").css({paddingTop: '10px'}) 
+				
+				ajaxLoad(xmlpath,buildMenu,'xml/loadMenu.xsl',['#'])
+			});		
+	}
 		// if the class is flagged, just call ajaxLoad
 }
