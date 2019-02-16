27a84bbdfd7182d69a9aaf2360bb457e1888d809
Andrew Hope <andrewdhope@gmail.com>
2019-02-16 14:20:21 -0800

    bounce work

--- siteprep/scripts/jQueryEffects.js
+++ siteprep/scripts/jQueryEffects.js
@@ -1,8 +1,8 @@
 function jBounceUp() {
 		$("header").addClass("up")
-			.animate({paddingTop: '34.2px'}, "slow") /* padding here is [2*(height of the header element)+2*(body's top margin)] */
+			.animate({paddingTop: '54.2px'}, "slow") /* padding here is [2*(height of the header element)+2*(body's top margin)] */
 			.animate({bottom: '100%'}, "slow", function() {
-				$("header").removeClass("down").css({paddingTop: '0px'}) /* paddingTop effects needed to seamlessly switch the header element from absoute to static position. */
+				$("header").removeClass("down").css({paddingTop: '10px'}) /* paddingTop effects needed to seamlessly switch the header element from absoute to static position. */
 				ajaxLoad(xmlpath,buildMenu,'xml/loadMenu.xsl',['#'])
 			});
 			
