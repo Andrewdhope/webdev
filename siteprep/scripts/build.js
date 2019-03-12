054907af2dcfbb4471bc954ed42a9308d0e02fcd
Andrew Hope <andrewdhope@gmail.com>
2019-03-12 13:39:40 -0700

    pqa js

--- siteprep/scripts/build.js
+++ siteprep/scripts/build.js
@@ -1,14 +1,23 @@
+
+// ajaxLoad
+// standard ajax call using the XMLHttpRequest object
+// this function is used to retrieve both xml documents and xsl stylesheets
+//  by calling the ajaxLoad function a second time when a stylesheet is needed
 function ajaxLoad(path, callback, stylesheetPath, args) { 
 	var request, xmlDoc ;
 	request = new XMLHttpRequest();
 	request.onreadystatechange = function() {
 		if (request.readyState == 4 && request.status == 200) {
 			xmlDoc = request.responseXML;
-			args.unshift(xmlDoc);
+			args.unshift(xmlDoc); // the xml document will be the first argument, unless it is unshifted by an xsl stylesheet
 			if (stylesheetPath) {
+				// this passes the execution to getStylesheet, 
+				//  which will call ajaxLoad, unshift the stylesheet,
+				// 	and execute the callback (by hitting the else below)
 				getStylesheet(stylesheetPath, callback, args); 
 			}
 			else {
+				// called if no stylesheet is defined (or if the stylesheet is already unshifted)
 				callback.apply(null,args); // using apply to pass an array of arguments to the callback function
 			}
 		}
@@ -17,41 +26,53 @@ function ajaxLoad(path, callback, stylesheetPath, args) {
 	request.send();
 }	
 
+// getStylesheet
+// call ajaxLoad again to get an xsl doc and unshift it into the args parameter
+// the xsl stylesheet path takes the place of the xml document in this call, with the stylesheet parameter null 
+// (the original xml document is passed in to this function within the args parameter)
 function getStylesheet(stylesheetPath, callback, args) {
-	// call ajaxLoad again to get an xsl doc and unshift it into the args parameter
 	ajaxLoad(stylesheetPath, callback, null, args); 
 	return;
 }
 
+// buildMenu
+// as a callback function, this takes an xsl stylesheet in the first parameter, 
+//  and the xml document as the second
 function buildMenu(xslDoc, xmlDoc) {
 	var xsltProcessor, resultDocument, content, menu ;
 	xsltProcessor = new XSLTProcessor();
 	xsltProcessor.importStylesheet(xslDoc); 
 	resultDocument = xsltProcessor.transformToFragment(xmlDoc, document);
 	
+	// 'slide up, clear, and append'
+	
+	// \slide the content side up before re-loading the navigation menu
+	$("#content").slideUp("slow", function() {
+		$("#content").html(""); // clear content
+		$("#menu").slideUp("slow", function() {
+			// start showing the border after one of the menu options is first clicked
+			if (!document.getElementById("menu").classList.contains("border")) {
+				$("#menu").addClass("border")
+			}
+			$("#menu").html(""); // clear menu
+			$("#menu").append(resultDocument).slideDown(); // append
+			}); 
+		}
+	);
+	
+	// -- javascript replaced by jQuery -- // 
 	// content = document.getElementById("content");
 	// content.innerHTML = ""; // clear content whenever menu is reloaded
-	$("#content").slideUp("slow", function() {
-		$("#content").html("");
-		$("#menu").slideUp("slow", function() {
-				// start showing the border after one of the menu options is first clicked
-				if (!document.getElementById("menu").classList.contains("border")) {
-					$("#menu").addClass("border")
-			}
-			$("#menu").html("");
-			$("#menu").append(resultDocument).slideDown();
-			}); // 'slide up, clear, and append'
-		}
-	); // clear content whenever menu is reloaded
-	
 	// menu = document.getElementById("menu");
 	// menu.innerHTML = ""; // 'clear and append'
-	
  	// menu.appendChild(resultDocument); 
 	
 	return;
 }
 
+// buildContent // TODO: rename
+// as a callback function, this takes an xsl stylesheet in the first parameter, 
+//  and the xml document as the second
 function buildCareer(xslDoc, xmlDoc, sectionVal) {
 	// null check on the stylesheet
 	var xsltProcessor, resultDocument, content ;
@@ -60,8 +81,7 @@ function buildCareer(xslDoc, xmlDoc, sectionVal) {
 	xsltProcessor.setParameter(null,"sectionVal",sectionVal); 
 	resultDocument = xsltProcessor.transformToFragment(xmlDoc, document);
 	
-	// content = document.getElementById("content");
-	// content.innerHTML = ""; // 'clear and append'
+	// 'slide up, clear, and append'
 	$("#content").slideUp("slow", function() {
 		// start showing the border after one of the menu options is first clicked
 		if (!document.getElementById("content").classList.contains("border")) {
@@ -70,8 +90,11 @@ function buildCareer(xslDoc, xmlDoc, sectionVal) {
 		$("#content").html("");
 		$("#content").append(resultDocument).slideDown();
 		}
-	); // 'slide up, clear, and append'
+	); 
  	
+	// -- javascript replaced by jQuery -- // 
+	// content = document.getElementById("content");
+	// content.innerHTML = ""; // 'clear and append'
 	// content.appendChild(resultDocument); 
 	
 	return;
--- siteprep/scripts/jQueryEffects.js
+++ siteprep/scripts/jQueryEffects.js
@@ -1,18 +1,30 @@
+// this file contains functions that rely primarily on jQuery effects
+
+// jBounceUp
+// creates a bounce effect the first time a user clicks on the header
+// when the page loads, the header is centered vertically
+// when either link option is clicked, the header slides to the top of the screen,
+//  and the appropriate menu options load
 function jBounceUp() {
+	// if the header is already up, just load the menu
 	if (document.getElementsByTagName("header")[0].classList.contains("up")) {
 		ajaxLoad(xmlpath,buildMenu,'xml/loadMenu.xsl',['#']);
 	} else {
 		$("header").addClass("up")
 			
+			/* paddingTop effects are needed to seamlessly switch the header element from absoute to static position. */
 			/* padding here should equal (height of the header element)+2*(body's top margin)+2*(desired padding after animation) */
+			/* TODO: see if I can calculate the 87.2px value dynamically */
 			.animate({paddingTop: '87.2px'}, "slow")
 			
 			.animate({bottom: '100%'}, "slow", function() {
 				
 				/* paddingTop effects are needed to seamlessly switch the header element from absoute to static position. */
+				/* without proper padding, the header will have a little hitch at the end of its animation */
 				/* paddingBottom added just to give the next divs some breathing room. */
 				$("header").removeClass("down").css({"paddingTop": "10px", "paddingBottom": "10px"})
-				$("footer p").slideDown("100")
+				
+				$("footer p").slideDown("100") /* also deploy the footer */
 			});	
 				
 			ajaxLoad(xmlpath,buildMenu,'xml/loadMenu.xsl',['#']) // this will start before the header animation completes (but it takes a few)
