f2b9e9e96a19f524986b68427f7e3c44510b62f9
Andrew Hope <andrewdhope@gmail.com>
2019-03-16 15:37:26 -0700

    css vars

--- siteprep/scripts/jQueryEffects.js
+++ siteprep/scripts/jQueryEffects.js
@@ -14,8 +14,15 @@ function jBounceUp() {
 			
 			/* paddingTop effects are needed to seamlessly switch the header element from absoute to static position. */
 			/* padding here should equal (height of the header element)+2*(body's top margin)+2*(desired padding after animation) */
-			/* TODO: see if I can calculate the 87.2px value dynamically */
-			.animate({paddingTop: '87.2px'}, "slow")
+			/* get this padding value dynamically... */
+			
+			var styles = window.getComputedStyle(document.documentElement); // get all styles
+			var headerheight = styles.getPropertyValue('--header-height'); // convert css variable to js
+			var bodymargintop = styles.getPropertyValue('--body-margin-top'); // convert css variable to js
+			var totalpaddingint = parseInt(headerheight) + parseInt(bodymargintop) + 20; // convert to integers and add 20
+			var totalpaddingpx = totalpaddingint+"px"; // convert to string and append px
+			var animatePadding = {paddingTop: totalpaddingpx}; // make a jQuery PlainObject for the animate function
+			.animate(animatePadding, "slow");
 			
 			.animate({bottom: '100%'}, "slow", function() {
 				
