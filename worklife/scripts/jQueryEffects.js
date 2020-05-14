64d42d9de0358c5c2b3e6b3d10a7f9b2ef66166a
Andrew Hope <andrewdhope@gmail.com>
2020-05-13 23:00:49 -0700

    splash redesign

--- worklife/scripts/jQueryEffects.js
+++ worklife/scripts/jQueryEffects.js
@@ -41,11 +41,11 @@ function jBounceUp() {
 			
 			$("header").animate({bottom: '100%', transform: 'translateX(50%) translateY(0%)'}, "slow", function() {
 				$("centered").css({"paddingRight": '2vw'}); // broken
-				$("header").animate({width: '100%'}, "slow", function() { 
+				$("header").animate({width: '95%'}, "slow", function() { 
 					/* paddingBottom added just to give the next divs some breathing room. */
 					$("header").removeClass("down").css({"paddingTop": headerpaddingtop, "paddingBottom": headerpaddingbottom}) // headerpaddingtotal 
 					$("footer p").slideDown("100") /* also deploy the footer */
-					ajaxLoad(xmlpath,buildMenu,'xml/loadMenu.xsl',['#']) // this will start before the header animation completes (but it takes a few)
+					ajaxLoad(xmlpath,buildMenu,'xml/loadMenu.xsl',['#']) // takes a few seconds
 				});
 			});	
 	}
