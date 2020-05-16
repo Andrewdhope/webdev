751ac9a63ee39126132406006d62bfac38b3483e
Andrew Hope <andrewdhope@gmail.com>
2020-05-16 14:36:41 -0700

    refactor animation

--- worklife/scripts/jQueryEffects.js
+++ worklife/scripts/jQueryEffects.js
@@ -6,20 +6,18 @@
 // when either link option is clicked, the header slides to the top of the screen,
 //  and the appropriate menu options load
 function jBounceUp() {
-
-	/* paddingTop effects are needed to seamlessly switch the header element from absoute to static position */
-	/* without proper padding, the header will have a little hitch at the end of its animation */
-	/* paddingTop here should equal (height of the header element)+2*(body's top margin)+2*(desired padding after animation) */
-	/* building variables to convert existing css variables into JS so we can get this padding value dynamically... */
 	
-	/* sadly, the css variables are not well supported by IE so there is a workaround to hardcode paddingTop */
+	/* these css variables aren't as necessary after refactoring, but they're useful in theory */	
+	/* all they do now is dip the padding down during the initial animation */
+	/* could simplify this, but they also serve a purpose as a useful code sample for future reference */
+	/* sadly, the css 'custom properties' are not well supported by IE so there is a workaround to hardcode paddingTop */
 	
-	var styles = window.getComputedStyle(document.documentElement); // get all styles
-	var headerpaddingtop = styles.getPropertyValue('--header-padding-top'); // convert css variable to js
-	var headerpaddingbottom = styles.getPropertyValue('--header-padding-bottom'); // convert css variable to js
-	var totalpaddingint = parseInt(headerpaddingtop) + 2*parseInt(headerpaddingbottom) // controls how much the header dips before rising
-	var totalpaddingvh = totalpaddingint+"vh"; // convert to string and append vh
-	var animatePadding = {paddingTop: totalpaddingvh}; // make a jQuery PlainObject for the animate function
+	let styles = window.getComputedStyle(document.documentElement); // get all styles
+	let headerpaddingtop = styles.getPropertyValue('--header-padding-top'); // convert css variable to js
+	let headerpaddingbottom = styles.getPropertyValue('--header-padding-bottom'); // convert css variable to js
+	let totalpaddingint = parseInt(headerpaddingtop) + 2*parseInt(headerpaddingbottom) // controls how much the header dips before rising
+	let totalpaddingvh = totalpaddingint+"vh"; // convert to string and append vh
+	let animatePadding = {paddingTop: totalpaddingvh}; // make a jQuery PlainObject for the animate function
 
 	// if the header is already up, just load the menu
 	if (document.getElementsByTagName("header")[0].classList.contains("up")) {
@@ -31,8 +29,8 @@ function jBounceUp() {
 		// *sigh* IE workaround
 		if (window.ActiveXObject !== undefined)
 		{
-			$("header").animate({paddingTop: "44vh"}, "slow"); // need to hardcode the paddingTop value for IE
-			// ...one problem with hardcoding is you can't really trace why its set that way
+			$("header").animate({paddingTop: "44vh"}, "slow"); // need to hardcode the paddingTop value for IE (from totalpaddingvint) 
+	
 		}
 		else // better browsers
 		{
