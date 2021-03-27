0444a8aed6b996eb38ceb81b138c9b4e554dd174
andrewdhope <andrewdhope@gmail.com>
2021-03-27 16:51:08 -0700

    mobile vh

--- worklife/scripts/vars.js
+++ worklife/scripts/vars.js
@@ -7,7 +7,7 @@ function vhViewport() {
 	let vh = window.innerHeight * 0.01; 
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 	window.addEventListener('resize', () => {
-		let vh = window.innerHeitght * 0.01; 
+		let vh = window.innerHeight * 0.01; 
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
 	});
 }
